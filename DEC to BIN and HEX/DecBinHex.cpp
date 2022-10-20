90feed2be70bfe97be46b03e34e793f5e72a4979
Vinok_yt86@top-academy.ru <igorvinokurov1984@gmail.com>
2022-10-20 16:21:56 +0300

    Dec to Bin

---
+++ DEC to BIN and HEX/DecBinHex.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+    setlocale (LC_ALL, "Rus");
+
+    // Из DEC в BIN
+    int dec; int bin = 0; int buff = 1;
+    cout << "Введите десятичное число: "; cin >> dec;
+    
+    while (dec)
+    {
+        bin += (dec % 2) * buff;
+        buff *= 10;
+        dec /= 2;
+    }
+    cout << bin << endl;
+}
